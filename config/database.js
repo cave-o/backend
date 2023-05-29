4d44d0449c92bf942a45ab0c3724971a5fac874a
cave-o <97689028+cave-o@users.noreply.github.com>
2023-05-29 22:48:36 +0000

    modified:   config/database.js

--- config/database.js
+++ config/database.js
@@ -16,11 +16,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('PGHOST', '127.0.0.1'),
-      port: env.int('PGPORT', 5432),
-      database: env('PGDATABASE', 'strapi_db'),
-      user: env('PGUSER', 'strapi'),
-      password: env('PGPASSWORD', 'kejadb'),
+      host: env('PGHOST', 'containers-us-west-165.railway.app'),
+      port: env.int('PGPORT', 6546),
+      database: env('PGDATABASE', 'railway'),
+      user: env('PGUSER', 'postgres'),
+      password: env('PGPASSWORD', 'abhtZtpOoqXvg4EUnyJb'),
       schema: env('DATABASE_SCHEMA', 'public'), // Optional
       ssl: env.bool(true),
     },

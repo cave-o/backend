e8127a65f340949eff897694d72f81f7869e2494
cave-o <97689028+cave-o@users.noreply.github.com>
2023-05-29 22:59:37 +0000

    modified:   config/database.js

--- config/database.js
+++ config/database.js
@@ -16,7 +16,7 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: env('PGHOST', 'containers-us-west-165.railway.app'),
+      host: env('PGHOST', '0.0.0.0'),
       port: env.int('PGPORT', 6546),
       database: env('PGDATABASE', 'railway'),
       user: env('PGUSER', 'postgres'),
